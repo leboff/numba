6669c5e43ae639696662d59a0b870d17befca077
Bryan Leboff <leboff@gmail.com>
2012-03-28 10:24:05 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Numba::Application.config.secret_token = '6374a0cba6c3de86327c9041a1fa3219bd9df62368257c64d1871d8fa70fc3863ea925070ccc159438c03dce05ed9c2f58d6dac34ef77f279e822f22e9c3c793'

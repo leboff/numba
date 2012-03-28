6ea1d81c17949b11fa680985dd65a06728f76199
Bryan Leboff <leboff@gmail.com>
2012-03-28 11:50:09 -0400

    themed up a bit, added models for listings and info.. now gonna map out the data model

---
+++ test/unit/listing_item_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ListingItemTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/unit/listing_visibility_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ListingVisibilityTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
